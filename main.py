3dadd9e2e08e97758a0e208103305ce67bf5d96e
Jucian Decezare <63060456+JucianKaue@users.noreply.github.com>
2021-02-22 15:06:51 -0300

    Board ready

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Game:
                   f'Available gamemodes are: {avaible_gamemodes}')
 
     @staticmethod
-    def create_window(window_size=(), bg_color="000000"):
+    def create_window(window_size=(), bg_color='000000'):
         try:
             pygame.init()
         except:
@@ -19,10 +19,7 @@ class Game:
             return False
         try:
             if len(window_size) == 2:
-
-                screen = pygame.display.set_mode(window_size)
-                screen.fill(bg_color)
-
+                return pygame.display.set_mode(window_size)
             else:
                 print("ERROR! WIDOWN_SIZE is invalid.")
                 return False
@@ -57,6 +54,47 @@ class Board:
         self.board_img = [pygame.image.load(f"images/{gamemode}/board/board-dark.png"),
                           pygame.image.load(f"images/{gamemode}/board/board-light.png")]
 
+    def create_board(self, position, size_board_squares, screen):
+        size_board_pixels = 350
+        size_squares_pixels = size_board_pixels//size_board_squares
 
-game = Game(gamemode="city")
-game.create_window(window_size=(400, 400))
+        self.board_img = [pygame.transform.scale(self.board_img[0], (size_squares_pixels, size_squares_pixels)),
+                          pygame.transform.scale(self.board_img[1], (size_squares_pixels, size_squares_pixels))]
+
+        initial_position = position
+        position = [position[0], position[1]]
+
+        img = 0
+
+        for x in range(0, size_board_squares):
+            for y in range(0, size_board_squares):
+
+                screen.blit(self.board_img[img % 2], position)
+
+                img += 1
+                position[0] += size_squares_pixels
+
+            img += 1
+
+            position[0] = initial_position[0]
+            position[1] += size_squares_pixels
+
+        pygame.display.update()
+
+        board = []
+        for x in range(0, size_board_squares):
+            for y in range(0, size_board_squares):
+                board.append((x, y))
+
+        return board
+
+
+gamemode = "city"
+
+game = Game(gamemode=gamemode)
+
+screen = game.create_window(window_size=(400, 500))
+board = Board(gamemode=gamemode).create_board(position=(25, 50), size_board_squares=10, screen=screen)
+
+
+pygame.time.wait(10000)
